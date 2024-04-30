9c24a891d9c5b0716b4dcffdd74d02f15ac90810
Akash9698 <senthilsenthil98832@gmail.com>
2024-04-30 18:09:32 +0530

    done

---
+++ src/components/LikeImage.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import HOC from './HOC'
+
+function LikeImage(props) {
+
+
+  const {likeImageCounter,handleLikeImageCount}=props
+  // const [likeImageCounter, setLikeImageCounter] = useState(0);
+
+  // const handleLikeImageCount = ()=>{
+  //   setLikeImageCounter(likeImageCounter+1);
+  // }
+
+  return (
+    <div>
+      <button onClick={handleLikeImageCount}>Like Image {likeImageCounter}</button>
+    </div>
+  )
+}
+
+export default HOC(LikeImage)
